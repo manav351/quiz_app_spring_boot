7f9e51d4f0ecc40ab5f2ec2c1ede8ce5e80ef58c
manav351 <manav12121999@gmail.com>
2023-07-21 03:03:48 +0530

    Adding Spring Boot project files from spring-initializer

---
+++ src/test/java/com/project/programmingQuiz/ProgrammingQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.programmingQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
